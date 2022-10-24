6bdf696c1253c32921d4c57ae876451bdb5c211d
Roy K. Maestre <roy.maestre@udea.edu.co>
2022-10-24 01:59:08 -0500

    MEMAME

--- Assets/_Game/Scripts/Cards/UI/HandController.cs
+++ Assets/_Game/Scripts/Cards/UI/HandController.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class HandController : MonoBehaviour
 {
     [SerializeField] private GameObject cardPrefab;
-    [SerializeField] private float duration = 1;
+    [SerializeField] private float duration = 0.6f;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private GameObject emptyCard;
+    [SerializeField] private float animationTime;
+    [SerializeField] private AnimationCurve lt;
     private List<GameObject> cardObjects;
     private Queue<Card> incomingCards;
     private bool isDrawing;
@@ -16,14 +20,14 @@ public class HandController : MonoBehaviour
         incomingCards = new Queue<Card>();
         wait = new WaitForSeconds(duration);
         cardObjects = new List<GameObject>();
-        CombatSingletonManager.Instance.eventManager.OnCardChange += RenderCard;
+        CombatSingletonManager.Instance.eventManager.OnCardChange += AddCard;
         CombatSingletonManager.Instance.eventManager.OnHandUpdate += UpdateHand;
         CombatSingletonManager.Instance.eventManager.OnCardDiscard += DiscardCard;
     }
     public void OnDisable()
     {
         cardObjects = null;
-        CombatSingletonManager.Instance.eventManager.OnCardChange -= RenderCard;
+        CombatSingletonManager.Instance.eventManager.OnCardChange -= AddCard;
         CombatSingletonManager.Instance.eventManager.OnHandUpdate -= UpdateHand;
         CombatSingletonManager.Instance.eventManager.OnCardDiscard -= DiscardCard;
     }
@@ -48,7 +52,19 @@ public class HandController : MonoBehaviour
         isDrawing = true;
         while (incomingCards.Count > 0)
         {
-            //AnimateCard(incomingCards.Dequeue())
+            Card card = incomingCards.Dequeue();
+            var empty = Instantiate(emptyCard, transform);
+            var cardRenderer = Instantiate(cardPrefab, spawnPosition);
+            CardSpriteController controller = cardRenderer.GetComponent<CardSpriteController>();
+            controller.InitCardSprite(card);
+            cardRenderer.transform.position = new Vector3(cardRenderer.transform.position.x, empty.transform.position.y,0f);
+            cardRenderer.transform.SetParent(empty.transform);
+            
+            LeanTween.moveLocalX(cardRenderer,0f, animationTime).setEase(lt).setOnComplete(() => {
+                cardRenderer.transform.SetParent(transform);
+                Destroy(empty);});
+            
+            cardObjects.Add(cardRenderer);
             yield return wait;
         }
 
